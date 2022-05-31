c3f9f1ab0a14a4a4d661d626241b63c21fb2d3c5
Niklas <niklas@nokxs.de>
2022-05-31 22:46:24 +0200

    Add docker worker

---
+++ trello-backuper.docker/Program.cs
@@ -0,0 +1,10 @@
+using trello_backuper.docker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
