eb23a7bb4f0e6a58513c39d3a376a1e7d49edc44
Niklas <niklas@nokxs.de>
2022-06-03 23:59:58 +0200

    Cronjob support

--- trello-backuper.docker/Program.cs
+++ trello-backuper.docker/Program.cs
@@ -1,10 +1,13 @@
 using trello_backuper.cli;
 using trello_backuper.docker;
+using trello_backuper.lib;
 
 IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
+        services.AddSingleton<BackupCreator>();
+        services.AddSingleton<BackupCommand>();
         services.AddSingleton<BackupCli>();
     })
     .Build();
--- trello-backuper.docker/Worker.cs
+++ trello-backuper.docker/Worker.cs
@@ -1,32 +1,37 @@
-using Quartz;
-using Quartz.Impl;
+using CronSTD;
+using trello_backuper.cli;
 
 namespace trello_backuper.docker
 {
     public class Worker : BackgroundService
     {
+        private readonly CronDaemon _cronDaemon = new();
+        private readonly ILogger<Worker> _logger;
+        private readonly BackupCli _backupCli;
+
+        public Worker(ILogger<Worker> logger, BackupCli backupCli)
+        {
+            _logger = logger;
+            _backupCli = backupCli;
+        }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             var cron = Environment.GetEnvironmentVariable("CRON") ?? throw new ArgumentException("Please provide a valid cron expression. See readme for details.");
+            _logger.LogInformation($"Running with cron pattern {cron}");
 
-            var factory = new StdSchedulerFactory();
-            
-            var scheduler = await factory.GetScheduler(stoppingToken);
-            await scheduler.Start(stoppingToken);
+            _cronDaemon.AddJob(cron, CronAction);
+            _cronDaemon.Start();
+            await Task.Delay(-1, stoppingToken);
+        }
 
-            var job = JobBuilder.Create<BackupJob>()
-                .WithIdentity("myJob", "group")
-                .Build();
+        private async void CronAction()
+        {
+            var appKey = Environment.GetEnvironmentVariable("TRELLO_APP_KEY") ?? throw new ArgumentException("Please provide a valid trello app key. See readme for details.");
+            var token = Environment.GetEnvironmentVariable("TRELLO_TOKEN") ?? throw new ArgumentException("Please provide a valid trello token. See readme for details.");
 
-            var trigger = TriggerBuilder.Create()
-                .WithIdentity("myJob", "group")
-                .StartNow()
-                .WithCronSchedule(cron)
-                .ForJob("myJob", "group")
-                .Build();
-
-            await scheduler.ScheduleJob(job, trigger, stoppingToken);
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await _backupCli.Run(new[] { "--app-key", appKey, "--token", token, "backup", "/backup" });
         }
     }
 }
