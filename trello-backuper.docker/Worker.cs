a1355179834be02ef5299f3df99ee76913f9e071
Niklas Pfenninger <nokxs@users.noreply.github.com>
2022-08-17 22:43:21 +0200

    Add docker worker (#1)
    * Add docker worker
    
    * First docker implementation
    
    * Add quartz for scheduling
    
    * trigger does not work yet
    
    * Cronjob support
    
    * Backup can be run with docker compose
    
    * fix build

---
+++ trello-backuper.docker/Worker.cs
@@ -0,0 +1,62 @@
+using CronSTD;
+using trello_backuper.cli;
+
+namespace trello_backuper.docker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly CronDaemon _cronDaemon = new();
+        private readonly ILogger<Worker> _logger;
+        private readonly BackupCli _backupCli;
+
+        private bool isBackupRunning = false;
+
+        public Worker(ILogger<Worker> logger, BackupCli backupCli)
+        {
+            _logger = logger;
+            _backupCli = backupCli;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var cron = Environment.GetEnvironmentVariable("CRON") ?? throw new ArgumentException("Please provide a valid cron expression. See readme for details.");
+            _logger.LogInformation($"Running with cron pattern {cron}");
+
+            _cronDaemon.AddJob(cron, CronAction);
+            _cronDaemon.Start();
+
+            // Start first backup right away
+            CronAction();
+            
+            await Task.Delay(-1, stoppingToken);
+        }
+
+        private async void CronAction()
+        {
+            if (isBackupRunning)
+            {
+                _logger.LogWarning("{time}: Backup already running. Skipping this run...", DateTimeOffset.Now);
+                return;
+            }
+
+            try
+            {
+                isBackupRunning = true;
+                var appKey = Environment.GetEnvironmentVariable("TRELLO_APP_KEY") ?? throw new ArgumentException("Please provide a valid trello app key. See readme for details.");
+                var token = Environment.GetEnvironmentVariable("TRELLO_TOKEN") ?? throw new ArgumentException("Please provide a valid trello token. See readme for details.");
+
+                _logger.LogInformation("--> Backup started at {time}", DateTimeOffset.Now);
+                await _backupCli.Run(new[] { "--app-key", appKey, "--token", token, "backup", "/backup" });
+                _logger.LogInformation("--> Backup done at {time}", DateTimeOffset.Now);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Exception occurred during backup. This will not prevent the next run.");
+            }
+            finally
+            {
+                isBackupRunning = false;
+            }
+        }
+    }
+}
